fc4f5805ef8a9fbb5c571a4ac5df3b48b2a0c62a
Gavin Parker <gp14958@my.bristol.ac.uk>
2016-02-16 11:29:02 +0000

    Added angular-ui, made basic page

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-(function(){
-"use strict";
-console.log("hello");
+angular.module('webApp', ['ui.bootstrap', 'ngAnimate'])
+.controller('FeedCtrl', function($scope, $http){
+  $scope.test = 'hi';
+  $http.get('holidays.json').success(function(result){
+    $scope.holidays = result.holidays;
+    console.log($scope.holidays);
+  });
 
-
-
-
-
-}());
+});
